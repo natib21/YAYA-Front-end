8b66c191fbb90a81ec4a72e574cd89cc8d022040
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-08-24 14:27:52 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = ()=>{
+  return <div className="text-3xl font-bold bg-[#f8f8f8] ">
+       <header className="bg-white text-center p-4 shadow-md flex items-center justify-between gap-4 px-12">
+          <a href="/" className="text-2xl font-semibold text-blue-600">
+            <img src="https://yayawallet.com/images/logo.svg" alt="YayaWallet Logo" className="h-12 inline-block mr-2" />
+          </a>
+          <h2 className="text-[#0d2f88]">YaYa Wallet Transaction Dashboard</h2>
+          <div>
+             
+          </div>
+       </header>
+  </div>
+}
+export default App
