31a923206f5209f377d223ce8eb2c352a7aa7763
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-08-24 16:30:48 +0300

    modify search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,17 @@ const App = ()=>{
             </a>
             <div>
               <Search onSearchResults={handleSearchResults}/>
+              <div>
+                {searchResults?.data?.length > 0 && (
+                 <div>
+                  {searchResults?.data?.map((result, index) => (
+                    <div key={index} className="text-left text-sm text-gray-600 mt-2">
+                      {result.ID} - {result.Sender} to {result.Receiver}
+                    </div>
+                ))}
+                </div>
+                )}
+              </div>
             </div>
           </div>
          
@@ -32,16 +43,19 @@ const App = ()=>{
                 className="bg-white rounded-lg shadow-md p-6 border-l-4 border-blue-500 transition-transform transform hover:scale-105"
               >
                 <div className="font-bold text-lg text-gray-800">
-                  Transaction ID: <span className="font-normal">{transaction.Id}</span>
+                  Transaction ID: <span className="font-normal">{transaction.ID}</span>
                 </div>
                 <div className="text-sm text-gray-600 mt-2">
                   Sender: <span className="font-medium">{transaction.Sender}</span>
                 </div>
                 <div className="text-sm text-gray-600">
-                  Recipient: <span className="font-medium">{transaction.recipient}</span>
+                  Receiver: <span className="font-medium">{transaction.Receiver}</span>
                 </div>
                 <div className="text-sm text-gray-600">
-                  Amount: <span className="font-medium text-blue-500">{transaction.amount}</span>
+                  Amount: <span className="font-medium text-blue-500">{transaction.Amount}</span>
+                </div>
+                <div className="text-sm text-gray-600">
+                  Currency: <span className="font-medium text-blue-500">{transaction.Currency}</span>
                 </div>
               </div>
             ))}
