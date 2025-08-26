2e8d2db9aec2090918fda7baed4ade3774e7f666
Nathnael Zelalem <nathnaelzelalem@gmail.com>
2025-08-26 19:40:52 +0300

    update delete Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import { useState ,useCallback} from "react";
-import Header from "./pages/header/Header";
-import Transaction from "./pages/transaction/Transaction";
-const App = ()=>{
+import { useState, useCallback } from 'react';
+import Header from './pages/header/Header';
+import Transaction from './pages/transaction/Transaction';
+const App = () => {
   const [searchResults, setSearchResults] = useState([]);
-  const [searchDetails, setSearchDetails] = useState([]); 
+  const [searchDetails, setSearchDetails] = useState([]);
   const handleSearchResults = useCallback((data) => {
     setSearchResults(data);
   }, []);
 
-   const handleDetailClick = useCallback((transaction) => {
+  const handleDetailClick = useCallback((transaction) => {
     setSearchDetails([transaction]);
-    setSearchResults([]); 
+    setSearchResults([]);
   }, []);
-  const handleDeleteRMV =()=>{
+  const handleDeleteRMV = () => {
     setSearchDetails([]);
-   }
- console.log(searchResults.data)
-  return <div className="text-3xl font-bold bg-[#f8f8f8] ">
-             <Header handleSearchResults={handleSearchResults} 
-                  handleDetailClick={handleDetailClick}
-                  searchResults={searchResults}
-                   />
-         <main className="container mx-auto p-4">
-            <Transaction searchDetails={searchDetails} 
-                      handleDeleteRMV={handleDeleteRMV} 
-                       />
-      </main> 
-  </div>
-}
-export default App
+  };
+  console.log(searchResults.data);
+  return (
+    <div className="text-3xl font-bold bg-[#f8f8f8] ">
+      <Header
+        handleSearchResults={handleSearchResults}
+        handleDetailClick={handleDetailClick}
+        searchResults={searchResults}
+      />
+      <main className="container mx-auto p-4">
+        <Transaction
+          searchDetails={searchDetails}
+          handleDeleteRMV={handleDeleteRMV}
+        />
+      </main>
+    </div>
+  );
+};
+export default App;
--- src/pages/transaction/TransactionSearch.jsx
+++ src/pages/transaction/TransactionSearch.jsx
@@ -1,47 +1,59 @@
 const TransactionSearch = ({ transaction, handleDeleteRMV }) => {
-   
-  return <div>
-           {transaction.map((transaction) => (
-            <div  key={transaction.ID} 
-                className="relative bg-white rounded-lg shadow-md p-6 border-l-4 border-blue-500 "
-              >
-              <div>
-                <div className="font-bold text-lg text-gray-800">
-                  Transaction ID: <span className="font-normal">{transaction.ID}</span>
-                </div>
-                <div className="text-sm text-gray-600 mt-2">
-                  Sender: <span className="font-medium">{transaction.Sender}</span>
-                </div>
-                <div className="text-sm text-gray-600">
-                  Receiver: <span className="font-medium">{transaction.Receiver}</span>
-                </div>
-                <div className="text-sm text-gray-600">
-                  Amount: <span className="font-medium text-blue-500">{transaction.Amount}</span>
-                </div>
-                <br />
-                <div className="text-sm text-gray-600">
-                  Currency: <span className="font-medium ">{transaction.Currency}</span>
-                </div>
-                <div className="text-sm text-gray-600">
-                  Cause: <span className="font-medium ">{transaction.Cause}</span>
-                </div>
-               </div>
-               <div>
-                <div className="text-sm text-gray-600 mt-2">
-                  Date: <span className="font-medium">{new Date(transaction.CreatedAt).toLocaleString()}</span>
-                </div>
-                <div className="text-sm text-gray-600">
-                  Status: <span className="font-medium">{transaction.Status}</span>
-                </div>
-               </div>
-               <div className="absolute top-3 right-3 flex items-center justify-center w-8 h-8 rounded-full 
-                      text-gray-500 hover:text-white hover:bg-red-500 cursor-pointer transition-all duration-200"
-                       onClick={handleDeleteRMV}>
-                 <span className="text-lg font-bold">×</span>
-               </div>
-              </div>
-            ))}
+  return (
+    <div>
+      {transaction.map((transaction) => (
+        <div
+          key={transaction.ID}
+          className="relative bg-white rounded-lg shadow-md p-6 border-l-4 border-blue-500 "
+        >
+          <div>
+            <div className="font-bold text-lg text-gray-800">
+              Transaction ID:{' '}
+              <span className="font-normal">{transaction.ID}</span>
+            </div>
+            <div className="text-sm text-gray-600 mt-2">
+              Sender: <span className="font-medium">{transaction.Sender}</span>
+            </div>
+            <div className="text-sm text-gray-600">
+              Receiver:{' '}
+              <span className="font-medium">{transaction.Receiver}</span>
+            </div>
+            <div className="text-sm text-gray-600">
+              Amount:{' '}
+              <span className="font-medium text-blue-500">
+                {transaction.Amount}
+              </span>
+            </div>
+            <br />
+            <div className="text-sm text-gray-600">
+              Currency:{' '}
+              <span className="font-medium ">{transaction.Currency}</span>
+            </div>
+            <div className="text-sm text-gray-600">
+              Cause: <span className="font-medium ">{transaction.Cause}</span>
+            </div>
           </div>
-  
-}
-export default TransactionSearch
+          <div>
+            <div className="text-sm text-gray-600 mt-2">
+              Date:{' '}
+              <span className="font-medium">
+                {new Date(transaction.CreatedAt).toLocaleString()}
+              </span>
+            </div>
+            <div className="text-sm text-gray-600">
+              Status: <span className="font-medium">{transaction.Status}</span>
+            </div>
+          </div>
+          <div
+            className="absolute top-3 right-3 flex items-center justify-center w-8 h-8 rounded-full 
+                      text-gray-500 hover:text-white hover:bg-red-500 cursor-pointer transition-all duration-200"
+            onClick={handleDeleteRMV}
+          >
+            <span className="text-lg font-bold">×</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default TransactionSearch;
